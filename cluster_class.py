c32994aaa015a920a4db72fa8add73d1f224b235
olgaborodina <ol.borodina@icloud.com>
2022-10-21 10:50:40 +0200

    Update changes as in .ipynb file

--- cluster_class.py
+++ cluster_class.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import numpy as np
 
 #database handling
@@ -80,8 +79,7 @@ class cluster():
         cart_center_coords = eq_center_coords.cartesian.x, eq_center_coords.cartesian.y, eq_center_coords.cartesian.z
         
         
-        d = dist(cart_center_coords[0], cart_center_coords[1],
-                 cart_coords[0], cart_coords[1])
+        d = dist(cart_center_coords[0], cart_center_coords[1], cart_coords[0], cart_coords[1])
         
         self.cart_coords = cart_coords
         self.cart_center_coords = cart_center_coords
@@ -95,7 +93,7 @@ class cluster():
 
             ax = plt.gca()
             c_max = plt.Circle((self.cart_center_coords[0].value,self.cart_center_coords[1].value), radius=np.max(self.center_dist.value),
-                                edgecolor='k',linestyle='-.', fill=False, label='max_dist')
+                                edgecolor='k', linestyle='-.', fill=False, label='max_dist')
             c_mean = plt.Circle((self.cart_center_coords[0].value,self.cart_center_coords[1].value), radius=np.mean(self.center_dist.value),
                                 edgecolor='gray', linestyle='--', fill=False, label='mean_dist')
             ax.add_artist(c_mean)
@@ -117,15 +115,15 @@ class cluster():
         
         
         # Error propagation
-        d_d = lambda plx, eplx: (1000)*((1/plx)**2.*(eplx))
+        d_d = lambda plx, eplx: (1000) * ((1 / plx) ** 2. * (eplx))
         np.random.seed(None)
         #distance in pc 
-        dist = 1000/self.sources['plx']
+        dist = 1000 / self.sources['plx']
         #distance errors
         dist_err = d_d(self.sources['plx'], self.sources['e_plx'])
         
         #number of realisations of the CDF
-        N=100
+        N = 100
         
         CDF_many = np.zeros((len(self.sources['RA_ICRS']),N))
         CDF_x_many = np.zeros((len(self.sources['RA_ICRS']),N))
@@ -140,31 +138,29 @@ class cluster():
             #Uncomment code to include error in cluster centre
             #centra_draw = [] 
             #centdec_draw = []
+
             centdist_draw = []
             for i in range(len(ra)):
                 dist_draw.append(np.random.normal(distance[i], edist[i], size=1)[0])
                 ra_draw.append(np.random.normal(ra[i], era[i], size=1)[0])
                 dec_draw.append(np.random.normal(dec[i], edec[i], size=1)[0])
-                #print(dist_draw, ra_draw, dec_draw)
                 #Uncomment to include error in cluster
                 #centra_draw.append(np.random.normal(centra, ecentra, size=1)[0])
                 #centdec_draw.append(np.random.normal(centdec, ecentdec, size=1)[0])
                 #centdist_draw.append(np.random.normal(centdist, ecentdist, size=1)[0])
-        #     print(ra_draw)
         
-            try1= SkyCoord(ra=ra_draw*u.degree, dec=dec_draw*u.degree, distance=dist_draw*u.Parcsec, frame='icrs')
-            try1 = try1.cartesian.x,try1.cartesian.y,try1.cartesian.z
-            tryc1= SkyCoord(ra=centra*u.degree, dec=centdec*u.degree, distance=centdist*u.Parcsec, frame='icrs')
-            tryc1 = tryc1.cartesian.x,tryc1.cartesian.y,tryc1.cartesian.z
+            try1= SkyCoord(ra=ra_draw * u.degree, dec=dec_draw * u.degree, distance=dist_draw * u.Parcsec, frame='icrs')
+            try1 = try1.cartesian.x, try1.cartesian.y, try1.cartesian.z
+            tryc1= SkyCoord(ra=centra * u.degree, dec=centdec * u.degree, distance=centdist * u.Parcsec, frame='icrs')
+            tryc1 = tryc1.cartesian.x, tryc1.cartesian.y, tryc1.cartesian.z
         
         
         
             #getting the distance of stars from the center of clusters
-            def dist(x1,y1,x2,y2):
-                return ((x2-x1)**2+(y2-y1)**2)**0.5
+            def dist(x1, y1, x2, y2):
+                return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
         
-            dtry1 = dist(tryc1[0],tryc1[1],
-                     try1[0],try1[1])
+            dtry1 = dist(tryc1[0], tryc1[1], try1[0], try1[1])
         
             CDF_x_try1 = np.sort(dtry1.value)
             CDFtry1 = np.arange(1, len(dtry1) + 1) / len(dtry1)  #normalised CDF
@@ -178,8 +174,8 @@ class cluster():
         for i in range(N):
             CDF_x_many[:,i], CDF_many[:,i] = make_a_CDF(self.sources['RA_ICRS'], self.sources['DE_ICRS'], dist, 
                                               self.sources['e_RA_ICRS'], self.sources['e_DE_ICRS'], dist_err,
-                                                       self.param['RA_ICRS'], self.param['DE_ICRS'],
-                                                        1000/self.param['plx'],d_d(self.param['plx'], self.param['s_plx']))
+                                              self.param['RA_ICRS'], self.param['DE_ICRS'],
+                                              1000 / self.param['plx'], d_d(self.param['plx'], self.param['s_plx']))
             
         #Uncomment to plot realisations.
         #     plt.plot(CDF_x_many[:,i], CDF_many[:,i], alpha=0.05, c="red")
@@ -187,7 +183,7 @@ class cluster():
         # for each data point compute the standard devation of all the realisations at that value of x
         for k in range(N):
                 
-            CDF_interp_func = scipy.interpolate.interp1d(CDF_x_many[:,k],CDF_many[:,k], fill_value="extrapolate")
+            CDF_interp_func = scipy.interpolate.interp1d(CDF_x_many[:,k], CDF_many[:,k], fill_value="extrapolate")
             CDF_err.append(CDF_interp_func(x_axis))
                     
         actual_errors = np.std(np.array(CDF_err), axis=0)
@@ -211,12 +207,12 @@ class cluster():
             rho_P = rho_0 * ((1 + (r * r / a / a)) ** -2.5)
             return rho_P
         
-        def Zhao_profile(r, rho_0, a, beta, gamma):
-            rho = rho_0 * ((r / a) ** -gamma) * ((1 + (r / a) ** 2) ** ((gamma - beta) / 2))
+        def Zhao_profile(r, rho_0, a, gamma):
+            rho = rho_0 * ((r / a) ** -gamma) * ((1 + (r / a) ** 2) ** ((gamma - 4.5) / 2))
             return rho
         
-        def dist(x1,y1,x2,y2):
-            return ((x2-x1)**2+(y2-y1)**2)**0.5
+        def dist(x1, y1, x2, y2):
+            return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
         
         def pdf2cdf(r, profile, *args):
             
@@ -233,9 +229,9 @@ class cluster():
             else:
                 print('Please choose a profile between King, Plummer or Zhao')
             
-            return np.cumsum(pdf)
+            return np.cumsum(pdf) / (np.cumsum(pdf).max()+1e-8)
         
-        def log_likelihood(theta,x,y,y_err,profile):
+        def log_likelihood(theta, x, y, y_err, profile):
             model = pdf2cdf(x, profile, *theta)
             sigma2 = y_err ** 2
             return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2)) #neg. chi-sq. + nuisance param
@@ -244,9 +240,9 @@ class cluster():
             if profile == 'King':
                 bounds = np.array([[0, 0, 10], [1, 10, np.inf]]).T
             elif profile == 'Plummer':
-                bounds = np.array([[0, 0], [np.inf, np.inf]]).T
+                bounds = np.array([[0, 0], [np.inf, 50]]).T
             elif profile == 'Zhao':
-                bounds = np.array([[0, 0, 0, 0], [np.inf, np.inf, np.inf, 7]]).T
+                bounds = np.array([[0, 0, 0], [np.inf, 50, 5]]).T
                                  
             #getting the args for minimise function
             nll = lambda *args: -log_likelihood(*args)
@@ -258,26 +254,26 @@ class cluster():
             profiles_dict = {
                 'King':['rho_0', 'r_c', 'r_t'],
                 'Plummer': ['rho_0', 'a'],
-                'Zhao':['rho_0', 'a', 'beta'],
+                'Zhao':['rho_0', 'a', 'gamma'],
             }
         
             if print_solution==True:
                 
-                print('-'*30)
-                print('Message: ',soln.message)
-                print('nhev: ',soln.nfev)
-                print('nit: ',soln.nit)
-                print('njev: ',soln.njev)
-                print('status: ',soln.status)
-                print('success: ',soln.success)
-                print('-'*30)
+                print('-' * 30)
+                print('Message: ', soln.message)
+                print('nhev: ', soln.nfev)
+                print('nit: ', soln.nit)
+                print('njev: ', soln.njev)
+                print('status: ', soln.status)
+                print('success: ', soln.success)
+                print('-' * 30)
         
                 print("Maximum likelihood estimates:")  
         
                 for i in range(0,len(profiles_dict[profile])):
                     print(f"{profiles_dict[profile][i]} = {soln.x[i]:.3f}")
         
-                print('-'*30)
+                print('-' * 30)
         
             else: 
                 pass
@@ -309,14 +305,11 @@ class cluster():
             if profile == 'King':
                 init = np.array([0.1, np.mean(self.center_dist.value), 5 * np.mean(self.center_dist.value)]) #rho_0, r_c, r_t
             elif profile == 'Plummer':
-                init = np.array([0.1, np.mean(self.center_dist.value)]) #rho_0, r_c, r_t
+                init = np.array([0.1, np.mean(self.center_dist.value)]) #rho_0, a
             elif profile == 'Zhao':
-                init = np.array([0.1, np.mean(self.center_dist.value), 2, 1]) #rho_0, r_c, r_t
-                
-            #init_kings = np.array([0.1, np.mean(d.value)]) #rho_0, r_c, r_t
+                init = np.array([0.1, np.mean(self.center_dist.value), 1]) #rho_0, a, gamma
             
-            
-            
+
             #finding solution by minimise alogorithm
             soln = mle(init_guess=init,
                         x=x_axis,
